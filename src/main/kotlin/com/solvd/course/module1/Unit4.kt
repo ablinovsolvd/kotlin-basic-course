f9f81f369a49c11e07c7b2b83fd3060516a509ff
Artyom Blinov <ablinov@solvd.com>
2025-08-12 11:36:24 +0200

    - added unit8

--- src/main/kotlin/com/solvd/course/module1/Unit4.kt
+++ src/main/kotlin/com/solvd/course/module1/Unit4.kt
@@ -10,10 +10,5 @@ fun task1() {
 }
 
 fun task2() {
-    println("Task 2")
-    val numbers = Array(3) { 1 }
-    println(numbers.contentToString())
-    for (i in numbers) {
-        print("$i \t")
-    }
+
 }
---
+++ src/main/kotlin/com/solvd/course/module1/practice/PracticeUnit1_3.kt
@@ -0,0 +1,2 @@
+package com.solvd.course.module1.practice
+
